8450bb6cd82fc30fdc057ec850b0f22e88fb3995
Amsterdance26 <amsterdance26@gmail.com>
2023-04-13 22:22:11 +0700

    Initial Commit

---
+++ SecondCourse_0_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondCourse_0_Practice
+//
+//  Created by Василий Васильевич on 13.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
